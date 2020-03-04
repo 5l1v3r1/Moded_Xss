0e3eb13770f1d49b036245c99d4dd75c36329e1a
Mr Moded <59224173+MrModed@users.noreply.github.com>
2020-03-04 22:13:52 +0300

    Add files via upload

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Moded_Xss
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_link = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btn_Execute = new System.Windows.Forms.Button();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_link
+            // 
+            this.txt_link.Location = new System.Drawing.Point(71, 11);
+            this.txt_link.Name = "txt_link";
+            this.txt_link.Size = new System.Drawing.Size(304, 20);
+            this.txt_link.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Link: ";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 49);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(429, 293);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Url";
+            this.columnHeader1.Width = 287;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Payload";
+            this.columnHeader2.Width = 136;
+            // 
+            // btn_Execute
+            // 
+            this.btn_Execute.Location = new System.Drawing.Point(381, 9);
+            this.btn_Execute.Name = "btn_Execute";
+            this.btn_Execute.Size = new System.Drawing.Size(60, 23);
+            this.btn_Execute.TabIndex = 4;
+            this.btn_Execute.Text = "Execute";
+            this.btn_Execute.UseVisualStyleBackColor = true;
+            this.btn_Execute.Click += new System.EventHandler(this.Btn_Execute_Click);
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.Location = new System.Drawing.Point(12, 359);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(429, 43);
+            this.lbl_status.TabIndex = 5;
+            this.lbl_status.Text = "Status: None";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 411);
+            this.Controls.Add(this.lbl_status);
+            this.Controls.Add(this.btn_Execute);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_link);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Xss Tester By MrModed";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txt_link;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button btn_Execute;
+        private System.Windows.Forms.Label lbl_status;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using RestSharp;
+
+namespace Moded_Xss
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Execute_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(txt_link.Text))
+            {
+                xssFound = 0;
+                btn_Execute.Enabled = false;
+
+                if (File.Exists("Xss.txt"))
+                {
+                    var xssPayloads = File.ReadAllLines("Xss.txt").ToList();
+                    foreach (var i in xssPayloads)
+                    {
+                        Thread t = new Thread(() => { 
+                        
+                        Task.Factory.StartNew(() => { xssVulnerableTest(txt_link.Text, i); }).Wait();
+                        });
+                        t.IsBackground = true;
+                        t.Start();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("file Xss.txt is not found !", "Warning !", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+        int xssFound = 0;
+
+        private void xssVulnerableTest(String Url, String Payload)
+        {
+            try
+            {
+                Task.Delay(1000);
+
+                var u = Url + Payload;
+                var client = new RestClient(u);
+                var req = new RestRequest(Method.GET);
+
+                lbl_status.Invoke((MethodInvoker)delegate { lbl_status.ForeColor = Color.Coral; lbl_status.Text = "Status: Testing " + u; });
+
+                var resp = client.Get(req);
+
+                if (resp.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    xssFound++;
+                    listView1.Invoke((MethodInvoker)delegate { listView1.Items.Add(new ListViewItem(new String[] { u, Payload })); });
+                }
+            }
+            catch { }
+        }
+    }
+}
